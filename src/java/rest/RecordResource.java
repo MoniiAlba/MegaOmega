11166c717bac1f055e000bab4952d66b939fa300
hca <hca@itam.mx>
2019-05-15 16:44:04 -0500

    Rest insertRecord y getRecords funciona

--- src/java/rest/RecordResource.java
+++ src/java/rest/RecordResource.java
@@ -5,10 +5,14 @@
  */
 package rest;
 
+import beans.RecordResponse;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
@@ -39,17 +43,49 @@ public class RecordResource {
     @GET
     @Produces("application/json")
     public String getHtml(
-    @QueryParam("dbName") String dbName) {
+        @QueryParam("dbName") String dbName, 
+        @QueryParam("tableName") String tableName, 
+        @QueryParam( "userName") String userName, 
+        @QueryParam("password") String password, 
+        @QueryParam( "start") int start, 
+        @QueryParam( "end") int end) {
         //TODO return proper representation object
-        return "{\"json\":\"true\"}";
+        return getRecords(dbName, tableName, userName, password, start, end);
     }
 
     /**
-     * PUT method for updating or creating an instance of RecordResource
+     * PUT INSERT RECORD
      * @param content representation for the resource
      */
     @PUT
-    @Consumes(MediaType.TEXT_HTML)
-    public void putHtml(String content) {
+    @Produces(MediaType.APPLICATION_XML)
+    public RecordResponse putHtml(
+        @FormParam("userName") String userName, 
+        @FormParam("tableName") String tableName, 
+        @FormParam( "values") String values, 
+        @FormParam("dbName") String dbName, 
+        @FormParam("password") String password) {
+        
+        boolean status = insertRecord(dbName, tableName, userName, password, values);
+        RecordResponse res;
+        if( status )
+            res = new RecordResponse("exito", "Insercion de tupla exitosa");
+        else
+            res = new RecordResponse("error", "Error en insercion de tupla");
+        return res;
     }
+
+    private static String getRecords(java.lang.String dbName, java.lang.String tableName, java.lang.String userName, java.lang.String password, int start, int end) {
+        tablewssc.TableService_Service service = new tablewssc.TableService_Service();
+        tablewssc.TableService port = service.getTableServicePort();
+        return port.getRecords(dbName, tableName, userName, password, start, end);
+    }
+
+    private static boolean insertRecord(java.lang.String dbName, java.lang.String tableName, java.lang.String userName, java.lang.String password, java.lang.String values) {
+        tablewssc.TableService_Service service = new tablewssc.TableService_Service();
+        tablewssc.TableService port = service.getTableServicePort();
+        return port.insertRecord(dbName, tableName, userName, password, values);
+    }
+    
+    
 }
